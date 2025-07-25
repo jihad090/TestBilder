57f99e3c8699afc44c44bb1960c5461883ebce61
dtech0 <sayedabdullahal11@gmail.com>
2025-07-25 20:57:06 +0600

    finished

--- src/app/Api/createQuestionPrimaryInfo/route.ts
+++ src/app/Api/createQuestionPrimaryInfo/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
       totalMark,
       examName,
       subject,
-      paper = "none",
+      paper = "",
       subjectCode,
       examType,
       totalTime,
